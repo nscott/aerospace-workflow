fe4ad7a847a9ea057d26ad9e1e57262be1bfac09
Nathan Scott <natedscott@gmail.com>
2026-01-28 09:13:21 -0800

    Simplify the script and workflow

--- script.sh
+++ script.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env bash
 
+export PATH="/opt/homebrew/bin:$PATH"
+
 # References:
 # - https://nikitabobko.github.io/AeroSpace/commands#list-windows
 # - https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
-result=$( \
-  aerospace list-windows --workspace visible --format "%{app-pid} %{window-id} %{app-name} %{window-title}" | \
-  while read -r appPid windowId appName windowTitle; \
-  do 
-    appPath="$(ps -o comm= -p "$appPid")"
-    bundlePath="${appPath%%\.app*}.app"
+# - https://github.com/yuriteixeira/aerospace-workflow
+aerospace list-windows --all --format "%{app-pid} %{window-id} %{app-name} %{window-title}" | \
+while read -r appPid windowId appName windowTitle; do
+  appPath="$(ps -o comm= -p "$appPid")"
+  bundlePath="${appPath%%\.app*}.app"
 
-    echo "{ 
-      \"title\": \"$appName\", 
-      \"subtitle\": $(echo "$windowTitle" | jq -Rsa .),
-      \"match\": $(echo "$appName | $windowTitle" | jq -Rsa .),
-      \"arg\": \"$windowId\", 
-      \"icon\": { 
-        \"type\": \"fileicon\", 
-        \"path\": \"$bundlePath\" 
-      } 
-    },";
-  done
-)
-
-echo "{ \"items\": [ $result ] }"
-
-if [ -n "$DEBUG" ]; then
-  echo ""
-  echo "--- END: $(date) ---"
-  echo ""
-fi
+  jq -n \
+    --arg title "$appName" \
+    --arg subtitle "$windowTitle" \
+    --arg match "$appName | $windowTitle" \
+    --arg arg "$windowId" \
+    --arg path "$bundlePath" \
+    '{
+      title: $title,
+      subtitle: $subtitle,
+      match: $match,
+      arg: $arg,
+      icon: { type: "fileicon", path: $path }
+    }'
+done | jq -s '{ items: . }'
